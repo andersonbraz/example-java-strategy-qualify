8f030b6db6c36c4800f2e0568dfc75238e671b06
Anderson Braz de Sousa <braz_anderson@msn.com>
2020-11-08 14:36:05 -0300

    Up File(s)

--- src/br/com/solution/main/Executor.java
+++ src/br/com/solution/main/Executor.java
@@ -3,7 +3,11 @@ package br.com.solution.main;
 import java.util.Arrays;
 import java.util.List;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import br.com.solution.model.Cliente;
+import br.com.solution.model.Segmento;
 
 public class Executor {
 
@@ -20,12 +24,20 @@ public class Executor {
 		c1.setCampoBeta(10);
 		c1.setCampoXadrez(10);
 		
+		Segmento s1 = new Segmento(c1);
+		String result1 = s1.getTipoSegmento().toString();
+		System.out.println(result1);
+		
 		c2.setNome("Gustavo");
 		c2.setEmail("gustavo@braz.com");
 		c2.setTelefone("(61)8888-7777");
 		c2.setCampoAlfa(700);
 		c2.setCampoBeta(10);
 		c2.setCampoXadrez(10);
+		
+		Segmento s2 = new Segmento(c2);
+		String result2 = s2.getTipoSegmento().toString();
+		System.out.println(result2);
 		
 		c3.setNome("Anderson");
 		c3.setEmail("anderson@braz.com");
@@ -34,10 +46,16 @@ public class Executor {
 		c3.setCampoBeta(478);
 		c3.setCampoXadrez(8010);
 		
+		Segmento s3 = new Segmento(c3);
+		String result3 = s3.getTipoSegmento().toString();
+		System.out.println(result3);
+		
 		List<Cliente> listaCliente = Arrays.asList(c1, c2, c3);
 		
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		String jsonResult = gson.toJson(listaCliente);
 		
-		System.out.println(listaCliente.toString());
+		System.out.println(jsonResult);
 		 
 
 	}
---
+++ src/br/com/solution/model/Segmento.java
@@ -0,0 +1,43 @@
+package br.com.solution.model;
+
+import br.com.solution.util.TipoSegmento;
+
+public class Segmento {
+	
+	public int campoAlfa;
+	public int campoBeta;
+	public int campoXadrez;
+	public TipoSegmento tipoSegmento;
+	
+	public Segmento(Cliente client) {
+		super();
+		this.campoAlfa = client.campoAlfa;
+		this.campoBeta = client.campoBeta;
+		this.campoXadrez = client.campoXadrez;
+	}
+	
+	public int getCampoAlfa() {
+		return campoAlfa;
+	}
+	public void setCampoAlfa(int campoAlfa) {
+		this.campoAlfa = campoAlfa;
+	}
+	public int getCampoBeta() {
+		return campoBeta;
+	}
+	public void setCampoBeta(int campoBeta) {
+		this.campoBeta = campoBeta;
+	}
+	public int getCampoXadrez() {
+		return campoXadrez;
+	}
+	public void setCampoXadrez(int campoXadrez) {
+		this.campoXadrez = campoXadrez;
+	}
+	
+	public TipoSegmento getTipoSegmento() {
+		
+		return TipoSegmento.TIPO_A;
+	}
+	
+}
