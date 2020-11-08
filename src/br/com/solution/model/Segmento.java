79c13d6d1be5e01f9353576b935e45d100f36a5b
Anderson Braz de Sousa <braz_anderson@msn.com>
2020-11-08 17:51:13 -0300

    Up File(s)

--- src/br/com/solution/model/Segmento.java
+++ src/br/com/solution/model/Segmento.java
@@ -19,25 +19,36 @@ public class Segmento {
 	public int getCampoAlfa() {
 		return campoAlfa;
 	}
+	
 	public void setCampoAlfa(int campoAlfa) {
 		this.campoAlfa = campoAlfa;
 	}
+	
 	public int getCampoBeta() {
 		return campoBeta;
 	}
+	
 	public void setCampoBeta(int campoBeta) {
 		this.campoBeta = campoBeta;
 	}
+	
 	public int getCampoXadrez() {
 		return campoXadrez;
 	}
+	
 	public void setCampoXadrez(int campoXadrez) {
 		this.campoXadrez = campoXadrez;
 	}
 	
 	public TipoSegmento getTipoSegmento() {
 		
+		System.out.println("check: " + this.campoAlfa);
+		System.out.println("check: " + this.campoBeta);
+		System.out.println("check: " + this.campoXadrez);
+		
+		
 		return TipoSegmento.TIPO_A;
+		
 	}
 	
 }
